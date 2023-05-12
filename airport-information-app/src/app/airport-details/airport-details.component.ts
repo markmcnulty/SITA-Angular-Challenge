5a4d6468e7cdb2f13d1b34017d52b178b7265530
Mark McNulty <markmcnulty84@hotmail.com>
2023-05-12 08:30:50 +0100

    Splitting the map into it's own component

--- airport-information-app/src/app/airport-details/airport-details.component.ts
+++ airport-information-app/src/app/airport-details/airport-details.component.ts
@@ -1,64 +1,32 @@
-import { OnInit, Component } from '@angular/core';
+import { Component } from '@angular/core';
 
-import { AirportService } from '../services/airport.service';
 import { ShareService } from '../services/share.service';
 import { ActivatedRoute } from '@angular/router';
 
 import flightInformation from '../../assets/mock-data/departures.json';
-import airportInformation from '../../assets/mock-data/full-airport-information.json';
 
 @Component({
   selector: 'app-airport-details',
   templateUrl: './airport-details.component.html',
   styleUrls: ['./airport-details.component.css'],
 })
-export class AirportDetailsComponent implements OnInit {
+export class AirportDetailsComponent {
   currentTime: Date = new Date();
   departures = flightInformation.departures;
   arrivals = flightInformation.arrivals;
-  dummyAirportInformation = airportInformation;
-  dummyData: any;
-  latitude: number = 53.4213;
-  longitude: number = -6.27007;
 
   airportInformation: any;
 
   constructor(
-    private airportService: AirportService,
     public shareService: ShareService,
     private route: ActivatedRoute
   ) {
     this.route.data.subscribe((data) => {
       this.airportInformation = data;
     });
-    this.latitude = this.airportInformation.data.location.lat;
-    this.longitude = this.airportInformation.data.location.lon;
 
     setInterval(() => {
       this.currentTime = new Date();
     }, 1000);
-  }
-
-  ngOnInit(): void {
-    //Uncomment to use dummy data
-    // this.dummyData = this.dummyAirportInformation.find(
-    //   (dd: any) => dd.iata === this.shareService.iataCode
-    // );
-
-    this.loadMap();
-  }
-
-  loadMap() {
-    const mapOptions = {
-      center: new google.maps.LatLng(this.latitude, this.longitude),
-      zoom: 8,
-      mapTypeId: google.maps.MapTypeId.ROADMAP,
-    };
-    const mapContainer = document.getElementById('map') as HTMLElement;
-    const map = new google.maps.Map(mapContainer, mapOptions);
-    const marker = new google.maps.Marker({
-      position: new google.maps.LatLng(this.latitude, this.longitude),
-      map: map,
-    });
   }
 }
---
+++ airport-information-app/src/app/google-map/google-map.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { AirportService } from '../services/airport.service';
+import { ShareService } from '../services/share.service';
+
+@Component({
+  selector: 'app-google-map',
+  templateUrl: './google-map.component.html',
+  styleUrls: ['./google-map.component.css'],
+})
+export class GoogleMapComponent implements OnInit {
+  airportInformation: any;
+
+  //defaulting to dublin
+  latitude: number = 53.4213;
+  longitude: number = -6.27007;
+
+  constructor(
+    private airportService: AirportService,
+    public shareService: ShareService
+  ) {}
+
+  async ngOnInit(): Promise<void> {
+    try {
+      this.airportInformation = await this.airportService
+        .getAirportInformationByIATA(this.shareService.iataCode)
+        .toPromise();
+      this.loadMap();
+    } catch (error) {
+      console.error('Error retrieving airport information:', error);
+    }
+  }
+
+  loadMap() {
+    const mapOptions = {
+      center: new google.maps.LatLng(
+        this.airportInformation.location.lat,
+        this.airportInformation.location.lon
+      ),
+      zoom: 8,
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+    };
+    const mapContainer = document.getElementById('map') as HTMLElement;
+    const map = new google.maps.Map(mapContainer, mapOptions);
+    const marker = new google.maps.Marker({
+      position: new google.maps.LatLng(this.latitude, this.longitude),
+      map: map,
+    });
+  }
+}
